0fa3ec53896cd16ffdcbac4d86fc1e2f9b9aad93
scewpsdev <jpg4859@gmail.com>
2024-11-01 16:09:46 +0100

    Initial commit

---
+++ src/GameState.cs
@@ -0,0 +1,26 @@
+﻿using Rainfall;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class GameState : State
+{
+	public override void init()
+	{
+	}
+
+	public override void destroy()
+	{
+	}
+
+	public override void update()
+	{
+	}
+
+	public override void draw(GraphicsDevice graphics)
+	{
+	}
+}
